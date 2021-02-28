7b204e62c8db47d0a781449b610e8a1c70e9e9b3
trung <phamtrungzm@gmail.com>
2021-02-28 20:11:50 +0700

    Add sample

--- Scripts/Startup.R
+++ Scripts/Startup.R
@@ -1,3 +1,12 @@
 print("this file was create within Rstudio")
 
 print("and now it on github")
+
+x <- 1
+print(x)
+x
+msg <- "hello"
+
+x <- 1:20 ##integer sequence
+
+##Data type: character, numberic(real number), integer, complex, loogical
