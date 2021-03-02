37d055d5681047bbc2cef8ce118510db488003dd
trung.phamthanh <trung.phamthanh@gameloft.com>
2021-03-02 11:18:19 +0700

    + Scope_rule

---
+++ Scripts/R_week2.R
@@ -0,0 +1,32 @@
+# f <- function(a,b=1, c=2, d = NULL){
+#   
+# }
+
+f <- function(a, b){
+  a * 2
+}
+
+f(2)
+
+myplot <- function(x,y,  type = "1", ...){
+  plot(x,y, type = type, ...)
+}
+
+args(paste)
+function(..., sep = " " , colapse = NULL){}
+
+lm <- function(x) {x * x}
+lm
+
+make.power <- function(n){
+  pow <- function(x){
+    x^n
+  }
+  pow
+}
+
+y <- 10
+f <- function(x) {
+  y <- 2
+  y^2 + g(x)
+}
