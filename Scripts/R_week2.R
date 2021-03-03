d74f6fcb1a58867698a89743f230e11cd81cfca4
trung.phamthanh <trung.phamthanh@gameloft.com>
2021-03-03 17:52:06 +0700

    + Add week3

--- Scripts/R_week2.R
+++ Scripts/R_week2.R
@@ -63,3 +63,56 @@ x-y
 
 x <- as.POSIXlt(x)
 x-y
+
+
+# Loop 
+# lapppy
+
+x <- list(a = 1:5, b = rnorm(10))
+lapply(x, mean)
+
+x <- 1:4
+lapply(x, runif, min = 0, max = 10)
+
+x <- list(a = matrix(1:4, 2, 2), b = matrix(1:6, 3, 2)) 
+lapply(x, function(elt) elt[,1])
+
+#apply
+x <- matrix(rnorm(200), 20, 10)
+apply(x, 2, mean)
+apply(x, 1, sum)
+apply(x, 1, quantile, probs = c(0.25, 0.75))
+a <- array(rnorm(2 * 2 * 10), c (2,2, 10))
+apply(a, c(1,2), mean)
+rowMeans(a, dims = 2)
+
+#mapply
+list(rep(1, 4), rep(2, 3), rep(3, 2), rep(4, 1))
+  #or
+mapply(rep, 1:4, 4:1)
+
+
+ noise <- function(n, mean, sd) {
+ rnorm(n, mean, sd)
+ }
+mapply(noise, 1:5, 1:5, 2)
+
+#tapply
+x <- c(rnorm(10), runif(10), rnorm(10, 1))
+f <- gl(3, 10)
+f
+tapply(x, f, mean)
+
+#split
+split(x,f)
+ #more than 1 level
+x <- rnorm(10)
+f1 <- gl(2, 5)
+f2 <- gl(5, 2)
+f1
+f2
+interaction(f1,f2)
+str(split(x, list(f1, f2)))
+
+
+invinsible(x)  # hide returned value on console
