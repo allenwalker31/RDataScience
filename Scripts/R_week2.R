645255d4104bea50224294ee579c3b7dfc0341df
trung.phamthanh <trung.phamthanh@gameloft.com>
2021-03-04 11:35:36 +0700

    Add week 4

--- Scripts/R_week2.R
+++ Scripts/R_week2.R
@@ -116,3 +116,43 @@ str(split(x, list(f1, f2)))
 
 
 invinsible(x)  # hide returned value on console
+
+
+#simulation
+
+dnorm(x, mean = 0, sd = 1, log = FALSE)
+pnorm(q, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
+qnorm(p, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
+rnorm(n, mean = 0, sd = 1)
+
+
+# simulate linear
+set.seed(20)
+x <- rnorm(100)
+e <- rnorm(100, 0 ,2)
+y <- 0.5 + 2 * x + e
+
+  # if x is  binary
+x <- rbinom( 100, 1, 0.5)
+e <- rnorm(100,0,2)
+y <- 0.5 + 2 * x + e
+summary(y)
+plot(x,y)
+
+  # poisson model
+
+set.seed(1)
+x <- rnorm(100)
+log.mu <- 0.5 + 0.3 * x
+y <- rpois(100, exp(log.mu))
+summary(y)
+plot(x,y)
+
+  #random sampling
+set.seed(1)
+sample(1:10, 4)
+sample(1:10, 4)
+sample(letters, 5)
+sample(1:10 )  #permutation
+sample(1:10)
+sample(1:10, replace =TRUE) #sample w/replacement
